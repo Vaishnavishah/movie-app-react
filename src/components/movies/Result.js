c8147947053f02489be12bcfe76ea748f7f6b679
Vaishnavishah <shahvaishnavi2411@gmail.com>
2023-04-18 14:36:45 -0400

    Added favourite feature for searched movies as well

--- src/components/movies/Result.js
+++ src/components/movies/Result.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {createFavourite} from "../../reducers/favourite-reducer";
+import {useDispatch} from "react-redux";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
 function Result({ result, openPopup }) {
+const dispatch = useDispatch();
+const handleHeartClick = () => {
+            if(result.imdbID) {
+                const newFavourite = {
+                        movieID: result.imdbID
+                      }
+                      dispatch(createFavourite(newFavourite));
+                      console.log(newFavourite);
+            }
+        }
 	return (
 		<div className="result" onClick={() => openPopup(result.imdbID)}>
-			<img src={result.Poster} />
+		    <div class="starContainer">
+			    <img src={result.Poster} />
+			    <span class="star"> <FontAwesomeIcon style ={{padding:"2px;"}} icon={solid('heart')} onClick={() => handleHeartClick()} /> </span>
+			</div>
 			<h3>{result.Title}</h3>
+
 		</div>
 	)
 }
