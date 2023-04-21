1f921e35f3d55dd2088f93d516bfffc7cc593ac8
Rajvi Abhangi <abhangirajvi@gmail.com>
2023-04-20 22:06:45 -0700

    call backend for search and search on input change

--- src/components/popular-movies/index.js
+++ src/components/popular-movies/index.js
@@ -8,8 +8,9 @@ import Search from "./Search";
 function PopularMovies() {
 
     const [homeState, setHomeState] = useState({results:[]});
-    const tmdbapiurl = "https://api.themoviedb.org/3/movie/popular?api_key=0d0e5e0e7c64ee865ffaf322789a7bda&language=en-US`";
     const omdbapiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
+    const searchUrl = "http://localhost:4000/api/search";
+    const movieUrl = "http://localhost:4000/api/movie";
     const [state, setState] = useState({
                                     s: "",
                                     results: [],
@@ -17,7 +18,7 @@ function PopularMovies() {
                                 });
 
     const popular = () => {
-        axios(tmdbapiurl).then(({ data }) => {
+        axios(searchUrl).then(({ data }) => {
 
                     let results = data.results;
                     setHomeState(prevState => {
@@ -30,29 +31,37 @@ function PopularMovies() {
     const handleInput = (e) => {
             let s = e.target.value;
             setState(prevState => {
-                            return { ...prevState, s: s }
-                        });
+                return { ...prevState, s: s }
+            });
+
+            s.length > 3 && axios(searchUrl, { params: {criteria: s}}).then(({ data }) => {
+                let results = data.Search;
+
+                setState(prevState => {
+                    return { ...prevState, results: results }
+                })
+            });
         }
 
-    const search = (e) => {
-            if (e.key === "Enter") {
-                if(state.s) {
-                    axios(omdbapiurl + "&s=" + state.s).then(({ data }) => {
-                        let results = data.Search;
+    // const search = (e) => {
+    //         if (e.key === "Enter") {
+    //             if(state.s) {
+    //                 axios(searchUrl, { params: {criteria: state.s}}).then(({ data }) => {
+    //                     let results = data.Search;
 
-                        setState(prevState => {
-                            return { ...prevState, results: results }
-                        })
-                    });
-                } else {
-                    setState({
-                             s: "",
-                             results: [],
-                             selected: {}
-                     });
-                }
-            }
-        }
+    //                     setState(prevState => {
+    //                         return { ...prevState, results: results }
+    //                     })
+    //                 });
+    //             } else {
+    //                 setState({
+    //                          s: "",
+    //                          results: [],
+    //                          selected: {}
+    //                  });
+    //             }
+    //         }
+    //     }
 
     const openPopup = id => {
             axios(omdbapiurl + "&i=" + id).then(({ data }) => {
@@ -68,15 +77,15 @@ function PopularMovies() {
                     return { ...prevState, selected: {} }
                 });
             }
-useEffect(() => {
-   popular();
- }, [])
+    useEffect(() => {
+    popular();
+    }, [])
 
 
     return (
         <div>
-            <Search handleInput={handleInput} search={search} />
-            {state.results.length > 0 ? <SearchResults results={state.results} openPopup={openPopup} /> :
+            <Search handleInput={handleInput} />
+            {state.results&& state.results.length > 0 ? <SearchResults results={state.results} openPopup={openPopup} /> :
             <Results results = {homeState.results} openPopup={openPopup} /> }
             {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
         </div>
