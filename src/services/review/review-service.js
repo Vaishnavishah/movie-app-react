7b220bddded8052c9fc4fff083796559747730f8
Vaishnavishah <shahvaishnavi2411@gmail.com>
2023-04-20 20:29:26 -0400

    Created movie review service and thunk

---
+++ src/services/review/review-service.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
+const REVIEW_API = `${SERVER_API_URL}/review`;
+
+
+const api = axios.create({ withCredentials: true });
+
+
+export const deleteReview = async (rid) => {
+  const response = await api.delete(`${REVIEW_API}/${rid}`);
+  return response.data
+}
+
+
+export const createReview = async (review) => {
+    const response = await api.post(`${REVIEW_API}`, review);
+    return response.data;
+}
+
+export const findReviewByMovie = async(mid) => {
+    const response = await api.get(`${REVIEW_API}/movie/${mid}`);
+    return response.data;
+}
+
+export const findReviewByUser = async(uid) => {
+    const response = await api.get(`${REVIEW_API}/user/${uid}`);
+    return response.data;
+}
+
+export const updateReview = async (review) => {
+    const response = await api.put(`${REVIEW_API}/${review._id}`, review);
+    return response;
+};
---
+++ src/services/review/review-thunk.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import * as reviewService from "./review-service";
+
+
+export const createReviewThunk = createAsyncThunk(
+  'review/createReview',
+  async (review) => {
+    const newReview = await reviewService.createReview(review)
+    return newReview
+});
+
+export const findReviewByUserThunk = createAsyncThunk(
+  'review/findReviewByUser', async (uid) =>
+    await reviewService.findReviewByUser(uid)
+);
+
+export const findReviewByMovieThunk = createAsyncThunk(
+  'review/findReviewByMovie', async (mid) =>
+    await reviewService.findReviewByMovie(mid)
+);
+
+export const deleteReviewThunk = createAsyncThunk(
+  'review/deleteReview',
+  async (rid) => {
+    await reviewService.deleteReview(rid)
+    return rid
+});
+
+export cont updateReviewThunk = createAsyncThunk(
+'review/updateReview',
+async (review) => {
+    await reviewService.updateReview(review);
+});
+
+
+
