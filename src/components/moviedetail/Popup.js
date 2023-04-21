e503603175a8361d5f56ed2f8878cd74e893ebdb
Vaishnavishah <shahvaishnavi2411@gmail.com>
2023-04-21 14:47:17 -0400

    Added reviewer username in reviewItem

--- src/components/moviedetail/Popup.js
+++ src/components/moviedetail/Popup.js
@@ -1,23 +1,35 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {createReview} from "../../reducers/review-reducer";
 import {useDispatch} from "react-redux";
 import {useSelector} from "react-redux";
 import ReviewList from "./Review-list";
 import {createReviewThunk} from "../../services/review/review-thunk";
+import {profileThunk} from "../../services/auth-thunks";
 
 
 function Popup({ selected, closePopup }) {
 let [writeReview, setWriteReview] = useState('');
   const dispatch = useDispatch();
   const review_list = useSelector(state => state.reviews)
+  const {currentUser} = useSelector(state => state.user)
+
+useEffect(() => {
+   dispatch(profileThunk())
+ }, [])
+
 const handleSubmit = () => {
+console.log("currentUser" + currentUser);
+if(currentUser) {
     const newReview = {
-            userID: 2,
+            userID: currentUser._id,
             review: writeReview,
             movieID: selected.imdbID
           }
           dispatch(createReviewThunk(newReview));
           console.log(newReview);
+          } else {
+            alert("Please login or sign up to write reviews for this movie!!");
+          }
 
 }
 
@@ -45,6 +57,8 @@ const handleSubmit = () => {
                 <button className = "submit" onClick = {handleSubmit}> Submit</button>
 
 				<button className="close" onClick={closePopup}>Close</button>
+				<br/>
+				<br/>
 				<ReviewList selected = {selected} />
 			</div>
 		</section>
--- src/components/moviedetail/ReviewItem.js
+++ src/components/moviedetail/ReviewItem.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {useDispatch, useSelector}
   from "react-redux";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import {getUser} from "../../services/auth-service"
 
 const ReviewItem = (
  {
@@ -13,14 +14,24 @@ const ReviewItem = (
             }
  }
 ) => {
+const [user, setUser] = useState({});
+const getUserById = async () => {
+        const response = await getUser(review.userID);
+        setUser(response);
+    }
+
+useEffect(() => {
+   getUserById()
+ }, [])
 
  return(
  <>
 
  <div className="container d-flex mt-2">
+        {user[0]?.username ? <span className="fw-bold"> @{user[0].username}: </span> : null}
         <span className="fw-bold">
-                                         {review.review}
-                                     </span>
+               &nbsp; {review.review}
+        </span>
   </div>
 </>
 
